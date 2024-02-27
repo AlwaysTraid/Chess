0cb721b3d0add194512e97c4cd1caeccdf57f414
Traid <alwaystraid@gmail.com>
2024-02-26 21:30:18 -0500

    Imported PlayChess.java + Bishop Functionality

--- chess/Bishop.java
+++ chess/Bishop.java
@@ -1,7 +1,6 @@
 package chess;
 
-import chess.Board;
-import chess.Color;
+import chess.*;
 
 public class Bishop extends Piece{
 
@@ -37,8 +36,38 @@ public class Bishop extends Piece{
             return false;
         }
     }
+    
+    public boolean canTravel(String oldPos, String newPos){
+        int filediff = newPos.charAt(0) - oldPos.charAt(0);
+        int rankdiff = newPos.charAt(1) - oldPos.charAt(1);
+
+        int fileTravelingRight = filediff > 0 ? 1 : -1;
+        int rankTravelingRight = rankdiff > 0 ? 1 : -1;
+
+        int file = oldPos.charAt(0) + fileTravelingRight;
+        int rank = oldPos.charAt(1) + rankTravelingRight;
+
+        while(file != newPos.charAt(0) && rank != newPos.charAt(1)){
+            char charfile = (char) file;
+            String pos = Character.toString(charfile) + Integer.toString(rank);
+            if(!((Board.ChessBoard.get(pos)).getDisplay().equals("  ") || (Board.ChessBoard.get(pos)).getDisplay().equals("##"))){
+                return false;
+            }
+        }
+        return true;
+    }
 
     public void move(String oldPos, String newPos){
+        char file = oldPos.charAt(0);
+        int rank = oldPos.charAt(1);
+        Piece oldBishopPiece = Board.ChessBoard.get(oldPos);
+        Board.ChessBoard.put(newPos, oldBishopPiece); // Moves Bishop To New Spot
         
+        //Change to either White/Black BlankSpace Piece
+        if(Board.getSquareColor(file, rank) == Color.WHITE){
+            Board.ChessBoard.put(oldPos, new BlankSpace("  ", Color.WHITE, file, rank));
+        }else{
+            Board.ChessBoard.put(oldPos, new BlankSpace("##", Color.BLACK, file, rank));
+        }
     }
 }
