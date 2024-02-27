88a4e245faae9c00d43e4e0964659407fccf07cb
Traid <alwaystraid@gmail.com>
2024-02-26 22:14:37 -0500

    Completed King Class - Doesn't have to travel, but messed up math behind it.

--- chess/King.java
+++ chess/King.java
@@ -21,8 +21,9 @@ public class King extends Piece{
 
     public boolean ableToMove(String oldPos, String newPos){
         if (
-        (oldPos.charAt(1) <= newPos.charAt(1)) ||
-        (oldPos.charAt(1) >= newPos.charAt(1))
+        ( (Math.abs(oldPos.charAt(0) - newPos.charAt(0))) == 0 && (Math.abs( oldPos.charAt(1) - newPos.charAt(1))) == 1 )||
+        ( (Math.abs(oldPos.charAt(0) - newPos.charAt(0))) == 1 && (Math.abs( oldPos.charAt(1) - newPos.charAt(1))) == 0 ) ||
+        ( (Math.abs(oldPos.charAt(0) - newPos.charAt(0))) == 1 && (Math.abs( oldPos.charAt(1) - newPos.charAt(1))) == 1 && (Math.abs(oldPos.charAt(0) - newPos.charAt(0))) == (Math.abs( oldPos.charAt(1) - newPos.charAt(1))) ) 
         ){
             if (Board.GetPiece(oldPos).getColor() == Board.GetPiece(newPos).getColor()){
                 return false;
@@ -35,7 +36,17 @@ public class King extends Piece{
     }
 
     public void move(String oldPos, String newPos){
+        char file = oldPos.charAt(0);
+        int rank = oldPos.charAt(1);
+        Piece oldKingPiece = Board.ChessBoard.get(oldPos);
+        Board.ChessBoard.put(newPos, oldKingPiece); // Moves King To New Spot
         
+        //Change to either White/Black BlankSpace Piece
+        if(Board.getSquareColor(file, rank) == Color.WHITE){
+            Board.ChessBoard.put(oldPos, new BlankSpace("  ", Color.WHITE, file, rank));
+        }else{
+            Board.ChessBoard.put(oldPos, new BlankSpace("##", Color.BLACK, file, rank));
+        }
     }
     
 }
